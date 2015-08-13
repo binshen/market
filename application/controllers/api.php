a2e41742737ed31eb7a6c40c3dfe180e6c3844fe
bin.shen <bluelazysb@hotmail.com>
2015-08-13 15:04:25 +0800

    123123

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,12 +1,11 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+if(!defined('APP_ID')) define('APP_ID', 'wx74ecabd9ee84aa89');
+if(!defined('APP_SECRET')) define('APP_SECRET', '03aae99bb72ec4fd2aa592e9716d762e');
+if(!defined('CB_TOKEN')) define('CB_TOKEN', '8654b302d5100247d2acc6211664c6f2');
+ 
 class Api extends MY_Controller {
-	
-	public $APP_ID = 'wx74ecabd9ee84aa89';
-	public $APP_SECRET = '03aae99bb72ec4fd2aa592e9716d762e';
-	
-	public $CALL_TOKEN = '8654b302d5100247d2acc6211664c6f2';
-	
+
 	public function __construct() {
 		parent::__construct();
 		
@@ -14,7 +13,63 @@ class Api extends MY_Controller {
 	}
 	
 	public function index() {
+		//$access_token = $this->api_model->get_or_create_token();
+		//var_dump($access_token);
 		
-		die("123");
+		$echoStr = $_GET["echostr"];
+		if(!empty($echoStr)) {
+			if($this->checkSignature()){
+				echo $echoStr;
+				exit;
+			}
+		} else {
+			$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+			if (!empty($postStr)){
+				$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+				$fromUsername = $postObj->FromUserName;
+				$toUsername = $postObj->ToUserName;
+				$keyword = trim($postObj->Content);
+				$time = time();
+				$textTpl = "
+					<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[%s]]></MsgType>
+						<Content><![CDATA[%s]]></Content>
+						<FuncFlag>0</FuncFlag>
+					</xml>
+				";
+				if(!empty( $keyword )) {
+					$msgType = "text";
+					$contentStr = "Welcome to wechat world!";
+					$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+					echo $resultStr;
+				}else{
+					echo "Input something...";
+				}
+			} else {
+				echo "";
+				exit;
+			}
+		}
+	}
+	
+	private function checkSignature() {
+		$signature = $_GET["signature"];
+		$timestamp = $_GET["timestamp"];
+		$nonce = $_GET["nonce"];
+	
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+	
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
--- application/models/api_model.php
+++ application/models/api_model.php
@@ -12,5 +12,36 @@ class Api_model extends MY_Model {
 		parent::__destruct();
 	}
 	
+	public function get_or_create_token() {
 		
+		$this->db->from('token');
+		$this->db->where('app_id', APP_ID);
+		$this->db->where('app_secret', APP_SECRET);
+		$data_token = $this->db->get()->row_array();
+		if(empty($data_token)) {
+			$data = array(
+				'app_id' => APP_ID,
+				'app_secret' => APP_SECRET,
+				'token' => $this->get_access_token(),
+				'created' => time()
+			);
+			$this->db->insert('token', $data);
+			return $data;
+		} else {
+			$interval = time() - intval($data_token['created']);
+			if($interval / 60 / 60 > 1) {
+				$data_token['token'] = $this->get_access_token();
+				$data_token['created'] = time();
+				$this->db->where('id', $data_token['id']);
+				$this->db->update('token', $data_token);
+			}
+			return $data_token;
+		}
+	}
+	
+	public function get_access_token() {
+		$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.APP_ID.'&secret='.APP_SECRET;
+		$response = file_get_contents($url);
+		return json_decode($response)->access_token;
+	}
 }
