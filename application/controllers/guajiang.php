6e130ae871a98e0cad68d05f496826d3d8956a01
bin.shen <bluelazysb@hotmail.com>
2015-08-31 13:36:34 +0800

    123123

--- application/controllers/guajiang.php
+++ application/controllers/guajiang.php
@@ -9,6 +9,16 @@ class Guajiang extends MY_Controller {
 	
 	public function index() {
 		
+		$redirect_uri = 'http://' . DOMAIN .'/guajiang/view';
+		$state = 'ggk_1';
+		$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.APP_ID.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state='.$state.'#wechat_redirect';
+		$result = file_get_contents(urlencode($url));
+		var_dump($result);
+		
+		
+	}
+	
+	public function view() {
 		
 		$this->display('guajiang.html');
 	}
