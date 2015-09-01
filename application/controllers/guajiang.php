02293ca237946a121b349b51433fe1627334c5cf
bin.shen <bluelazysb@hotmail.com>
2015-09-01 10:05:08 +0800

    123123

--- application/controllers/guajiang.php
+++ application/controllers/guajiang.php
@@ -9,13 +9,15 @@ class Guajiang extends MY_Controller {
 	
 	public function index() {
 		
-		if(empty($_GET['code'])){
-			$redirect_uri = 'http://' . DOMAIN .'/guajiang';
+		$code = $_GET['code'];
+		if(empty($code)){
+			$redirect_uri = 'http://' . DOMAIN .'/guajiang/';
 			$state = 'ggk_1';
 			$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.APP_ID.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state='.$state.'#wechat_redirect';
 			redirect(urlencode($url));
 		} else {
-			$result = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appid}&secret={$secret}&code={$_GET['code']}&grant_type=authorization_code");
+			$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.APP_ID.'&secret='.APP_SECRET.'&code='.$code.'&grant_type=authorization_code';
+			$result = file_get_contents($url);
 			$jsonInfo = json_decode($result,true);
 			//$access_token = $jsonInfo["access_token"];
 			//$openid = $jsonInfo["openid"];
